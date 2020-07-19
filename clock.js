672ae265c83f6fec98520753b55c0e9e152e5adc
nlom0218 <57981252+nlom0218@users.noreply.github.com>
2020-07-19 19:24:55 +0900

    Update clock.js
    Update clock.js

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+//clockContainer 즉 .js-clock의 자식 중 h1를 찾는다.
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
