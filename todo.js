55bf3bcb83ef0c79466fd6a0c699ddd9acb5897d
nlom0218 <57981252+nlom0218@users.noreply.github.com>
2020-07-21 06:51:41 +0900

    Making a To Do List
    Making a To Do List last part

--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoform = document.querySelector(".js-toDoform"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,6 +25,7 @@ function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteToDo);
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   span.innerText = text;
