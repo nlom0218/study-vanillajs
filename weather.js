39181acf74a312c554a36ab2e1431effa38f2af4
nlom0218 <57981252+nlom0218@users.noreply.github.com>
2020-07-21 22:27:49 +0900

    Getting the Weather part One Geolocation
    Getting the Weather part One Geolocation

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "58f5d487b06fd096821ef170a3d65d47";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("위치를 불러 올 수 없습니다.");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
